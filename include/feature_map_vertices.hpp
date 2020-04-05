58107729dd66b9fb487cb59aa22c0021783a40e4
Kohei Takada <khi.tkd@gmail.com>
2020-04-05 19:51:25 +0900

    Add multiple files loader function to feature_map_vertices.

--- include/feature_map_vertices.hpp
+++ include/feature_map_vertices.hpp
@@ -1,12 +1,15 @@
 #pragma once
+#include <Eigen/Dense>
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <memory>
+#include <experimental/filesystem>
 
-#include <Eigen/Dense>
+namespace fs = std::experimental::filesystem;
 
-// vertex index, timestamp [ns], position x [m], position y [m], position z [m], quaternion x, quaternion y, quaternion z, quaternion w
+// vertex index, timestamp [ns], translation x [m], translation y [m], translation z [m], quaternion x, quaternion y, quaternion z, quaternion w
 // velocity x [m/s], velocity y [m/s], velocity z [m/s]
 // acc bias x [m/s^2], acc bias y [m/s^2], acc bias z [m/s^2]
 // gyro bias x [rad/s], gyro bias y [rad/s], gyro bias z [rad/s]
@@ -16,14 +19,14 @@
 typedef struct Vertex_
 {
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-  Vertex_(long int index_, double timestamp_, Eigen::Vector3d position_, Eigen::Quaterniond rotation_,
+  Vertex_(long int index_, double timestamp_, Eigen::Vector3d translation_, Eigen::Quaterniond rotation_,
           Eigen::Vector3d velocity_, Eigen::Vector3d acc_bias_, Eigen::Vector3d gyro_bias_)
-      : index(index_), timestamp(timestamp_), position(position_), rotation(rotation_), velocity(velocity_), acc_bias(acc_bias_), gyro_bias(gyro_bias_)
+      : index(index_), timestamp(timestamp_), translation(translation_), rotation(rotation_), velocity(velocity_), acc_bias(acc_bias_), gyro_bias(gyro_bias_)
   {
   }
   long int index;
   double timestamp;
-  Eigen::Vector3d position;
+  Eigen::Vector3d translation;
   Eigen::Quaterniond rotation;
   Eigen::Vector3d velocity;
   Eigen::Vector3d acc_bias;
@@ -33,15 +36,19 @@ typedef struct Vertex_
 class FeatureMapVertices
 {
 public:
-  FeatureMapVertices(std::string path_to_csv);
+  FeatureMapVertices(const fs::path &file_path);
+  FeatureMapVertices(const std::vector<fs::path> &file_paths);
   ~FeatureMapVertices() {}
 
-  std::vector<std::shared_ptr<Vertex>> data() { return data_; }
+  std::vector<std::shared_ptr<Vertex>> vertices() { return vertices_; }
 
-  bool get_pose(double timestamp, Eigen::Vector3d &position, Eigen::Quaterniond &rotation);
+  bool get_pose(double timestamp, Eigen::Vector3d &translation, Eigen::Quaterniond &rotation);
   void print(void);
 
 private:
-  std::vector<std::vector<double>> table_;
-  std::vector<std::shared_ptr<Vertex>> data_;
+  std::vector<std::shared_ptr<Vertex>> vertices_;
+
+  size_t load_from_csv(const fs::path &file_path);
+  bool vertex_from_string_vector(const std::vector<std::string> &record, std::shared_ptr<Vertex> &vertex);
+  void sort_by_timestamp(void);
 };
--- src/feature_map_vertices.cpp
+++ src/feature_map_vertices.cpp
@@ -5,54 +5,28 @@
 #include <fstream>
 #include <sstream>
 
-FeatureMapVertices::FeatureMapVertices(std::string path_to_csv)
+#define NANOSECONDS_IN_A_SECOND 1.0e9
+
+FeatureMapVertices::FeatureMapVertices(const fs::path &file_path)
 {
-  std::fstream filestream(path_to_csv);
-
-  if (!filestream.is_open())
-    return;
-
-  std::string buffer;
-  while (getline(filestream, buffer))
-  {
-    std::vector<std::string> record;
-    std::istringstream streambuffer(buffer);
-    std::string token;
-
-    while (getline(streambuffer, token, ','))
-    {
-      record.push_back(token);
-    }
-
-    if (record.size() == NUMBER_OF_ELEMENTS_IN_A_VERTEX)
-    {
-      try
-      {
-        std::shared_ptr<Vertex> vertex = std::make_shared<Vertex>(
-            std::stol(record[0]),
-            std::stod(record[1]) / 1.0e9,
-            Eigen::Vector3d(std::stod(record[2]), std::stod(record[3]), std::stod(record[4])),
-            // Caution: w, x, y, z
-            Eigen::Quaterniond(std::stod(record[8]), std::stod(record[5]), std::stod(record[6]), std::stod(record[7])),
-            Eigen::Vector3d(std::stod(record[9]), std::stod(record[10]), std::stod(record[11])),
-            Eigen::Vector3d(std::stod(record[12]), std::stod(record[13]), std::stod(record[14])),
-            Eigen::Vector3d(std::stod(record[15]), std::stod(record[16]), std::stod(record[17])));
-        data_.push_back(vertex);
-      }
-      catch (const std::exception &e)
-      {
-        // std::cerr << e.what() << std::endl;
-        // std::cerr << "skip this line" << std::endl;
-      }
-    }
-  }
+  load_from_csv(file_path);
+  sort_by_timestamp();
 }
 
-bool FeatureMapVertices::get_pose(double timestamp, Eigen::Vector3d &position, Eigen::Quaterniond &rotation)
+FeatureMapVertices::FeatureMapVertices(const std::vector<fs::path> &file_paths)
+{
+  for (auto file_path : file_paths)
+  {
+    load_from_csv(file_path);
+  }
+  sort_by_timestamp();
+}
+
+bool FeatureMapVertices::get_pose(double timestamp, Eigen::Vector3d &translation, Eigen::Quaterniond &rotation)
 {
   std::shared_ptr<Vertex> before = nullptr;
   std::shared_ptr<Vertex> after = nullptr;
-  for (auto vertex : data_)
+  for (auto vertex : vertices_)
   {
     if (vertex->timestamp <= timestamp)
       before = vertex;
@@ -64,22 +38,77 @@ bool FeatureMapVertices::get_pose(double timestamp, Eigen::Vector3d &position, E
   if (before == nullptr || after == nullptr)
     return false;
   auto t = (timestamp - before->timestamp) / (after->timestamp - before->timestamp);
-  position = before->position * (1 - t) + after->position * t;
+  translation = before->translation * (1 - t) + after->translation * t;
   rotation = before->rotation.slerp(t, after->rotation);
   return true;
 }
 
 void FeatureMapVertices::print(void)
 {
-  std::cout << table_.size() << std::endl;
-  for (auto vertex : data_)
+  for (auto vertex : vertices_)
   {
     std::cout << vertex->index << ", ";
     std::cout.precision(20);
     std::cout << vertex->timestamp << ", ";
-    std::cout << vertex->position(0) << ", "
-              << vertex->position(1) << ", "
-              << vertex->position(2) << ", ";
+    std::cout << vertex->translation(0) << ", "
+              << vertex->translation(1) << ", "
+              << vertex->translation(2) << ", ";
     std::cout << std::endl;
   }
+}
+
+size_t FeatureMapVertices::load_from_csv(const fs::path &file_path)
+{
+  std::fstream filestream(file_path.string());
+
+  if (!filestream.is_open())
+    return 0;
+
+  std::string buffer;
+  while (getline(filestream, buffer))
+  {
+    std::vector<std::string> record;
+    std::istringstream streambuffer(buffer);
+    std::string token;
+
+    while (getline(streambuffer, token, ','))
+      record.emplace_back(token);
+
+    std::shared_ptr<Vertex> vertex;
+    if (record.size() == NUMBER_OF_ELEMENTS_IN_A_VERTEX &&
+        vertex_from_string_vector(record, vertex))
+      vertices_.emplace_back(vertex);
+  }
+  return vertices_.size();
+}
+
+bool FeatureMapVertices::vertex_from_string_vector(const std::vector<std::string> &record, std::shared_ptr<Vertex> &vertex)
+{
+  try
+  {
+    vertex = std::make_shared<Vertex>(
+        std::stol(record[0]),
+        std::stod(record[1]) / NANOSECONDS_IN_A_SECOND,
+        Eigen::Vector3d(std::stod(record[2]), std::stod(record[3]), std::stod(record[4])),
+        // Caution: w, x, y, z
+        Eigen::Quaterniond(std::stod(record[8]), std::stod(record[5]), std::stod(record[6]), std::stod(record[7])),
+        Eigen::Vector3d(std::stod(record[9]), std::stod(record[10]), std::stod(record[11])),
+        Eigen::Vector3d(std::stod(record[12]), std::stod(record[13]), std::stod(record[14])),
+        Eigen::Vector3d(std::stod(record[15]), std::stod(record[16]), std::stod(record[17])));
+    return true;
+  }
+  catch (const std::exception &e)
+  {
+    // std::cerr << e.what() << std::endl;
+    // std::cerr << "skip this line" << std::endl;
+    return false;
+  }
+}
+
+void FeatureMapVertices::sort_by_timestamp(void)
+{
+  std::sort(vertices_.begin(), vertices_.end(),
+            [&](auto const &a, auto const &b) {
+              return a->timestamp < b->timestamp;
+            });
 }
